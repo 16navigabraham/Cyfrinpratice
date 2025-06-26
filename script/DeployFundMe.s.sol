87305faa965de96fb858b9dbdfc8a930629416a2
Adebanjo <navigatorabraham17@gmail.com>
2025-06-26 16:52:56 +0100

    updated cyfrin fundme for pratice

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,18 +1,22 @@
-//SPDX-License-Identifier:MIT
-
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/script.sol";
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 import {FundMe} from "../src/FundMe.sol";
 
+contract DeployFundMe is Script {
+    function deployFundMe() public returns (FundMe, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+        address priceFeed = helperConfig.getConfigByChainId(block.chainid).priceFeed;
 
-contract DeployFundMe is Script{
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(priceFeed);
+        vm.stopBroadcast();
+        return (fundMe, helperConfig);
+    }
 
-function run() external returns (FundMe) {
-    vm.startBroadcast();
-    FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-    vm.stopBroadcast();
-    return fundMe
-
-}
+    function run() external returns (FundMe, HelperConfig) {
+        return deployFundMe();
+    }
 }
